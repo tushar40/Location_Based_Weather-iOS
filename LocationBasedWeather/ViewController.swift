7b9e8117e098468fb2383df8502d43aa159505eb
tushar40 <tushar@hotcocoasoftware.com>
2020-02-04 13:43:21 +0530

    Initial Commit

---
+++ LocationBasedWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationBasedWeather
+//
+//  Created by Tushar Gusain on 04/02/20.
+//  Copyright © 2020 Hot Cocoa Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LocationBasedWeatherTests/LocationBasedWeatherTests.swift
@@ -0,0 +1,34 @@
+//
+//  LocationBasedWeatherTests.swift
+//  LocationBasedWeatherTests
+//
+//  Created by Tushar Gusain on 04/02/20.
+//  Copyright © 2020 Hot Cocoa Software. All rights reserved.
+//
+
+import XCTest
+@testable import LocationBasedWeather
+
+class LocationBasedWeatherTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
